c355409c1acec71348207e3cb1f716591b815d8f
Pavol Celuch <pavol.celuch@udemy.com>
2020-04-19 18:53:38 +0100

    part2_d_exercises

--- part2/phonebook/src/app.js
+++ part2/phonebook/src/app.js
@@ -1,8 +1,8 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Persons from "./components/persons";
 import Filter from "./components/filter";
 import PersonForm from "./components/personForm";
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -10,10 +10,13 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [filterWord, setNewFilterWord] = useState("");
 
-  useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+  const reloadPersons = () =>
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
+
+  useEffect(() => {
+    reloadPersons();
   }, []);
 
   const onSubmitHandler = (event) => {
@@ -23,14 +26,33 @@ const App = () => {
       (person) => person.name === newName
     );
     if (isNewPersonInPersons) {
-      alert(`${newName} is already added to phonebook`);
-      return;
+      if (
+        window.confirm(
+          `${newName} is already added to the phonebook, replace the old number with a new one ?`
+        )
+      ) {
+        const existingPerson = persons.find(
+          (person) => person.name === newName
+        );
+        personService
+          .update(existingPerson.id, { ...existingPerson, number: newNumber })
+          .then(() => reloadPersons())
+          .then(() => {
+            setNewName("");
+            setNewNumber("");
+          });
+        return;
+      } else {
+        return;
+      }
     }
 
     const newPerson = { name: newName, number: newNumber };
-    setPersons(persons.concat(newPerson));
-    setNewName("");
-    setNewNumber("");
+    personService.create(newPerson).then((returnPerson) => {
+      setPersons(persons.concat(returnPerson));
+      setNewName("");
+      setNewNumber("");
+    });
   };
 
   const onChangeNameHandler = (event) => {
@@ -41,6 +63,11 @@ const App = () => {
   };
   const onChangeFilterHandler = (event) => {
     setNewFilterWord(event.target.value);
+  };
+  const onDeleteHandler = (person) => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService.deletePerson(person.id).then(() => reloadPersons());
+    }
   };
 
   const personsToShow = persons.filter((person) => {
@@ -71,7 +98,7 @@ const App = () => {
       <PersonForm {...personFormProps} />
 
       <h2>Numbers</h2>
-      <Persons persons={personsToShow} />
+      <Persons persons={personsToShow} onDeleteHandler={onDeleteHandler} />
     </>
   );
 };
--- part2/phonebook/src/components/persons.js
+++ part2/phonebook/src/components/persons.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, onDeleteHandler }) => {
   return (
     <>
       {persons.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
+          <button onClick={() => onDeleteHandler(person)}>delete</button>
         </div>
       ))}
     </>
